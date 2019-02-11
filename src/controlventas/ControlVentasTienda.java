f1ea07b3b617855c2e8e0e92849f1a2b4c661ef1
ChrisLopez <chris-lopez32@hotmail.com>
2019-02-10 23:13:45 -0600

    Inicializaci

---
+++ src/controlventas/ControlVentasTienda.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlventas;
+
+import controlventas.modelos.Cliente;
+/**
+ *
+ * @author chrisloops
+ */
+public class ControlVentasTienda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Cliente cliente = new Cliente();
+        System.out.println("Bienvenido al Sistema");
+    }
+    
+}
